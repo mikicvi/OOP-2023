2d8800e9764838f8eafdcbb2f9f0adcb2757277e
mikicvi <vilim.mikic@gmail.com>
2023-02-01 17:02:49 +0000

    Splash, end, score implemented

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -10,7 +10,9 @@ public class BugZap extends PApplet
 		size(500, 500);
 	}
 
-	public void setup() {
+	public void setup() 
+	{
+		
 		colorMode(HSB);
 		background(0);
 		playerX = 250;
@@ -19,41 +21,53 @@ public class BugZap extends PApplet
 
 
 		bugX = random(30, 470);
-		bugY = 0;
+		bugY = 20;
 		bugWidth = 30;
 	}
 
 	float playerX, playerY, playerHeight, playerWidth;
 	float bugX, bugY, bugWidth, bugHeight, bugInitX;
+	float score = 0;
+	int inGame = 0;
 
 
-	
+
 	public void draw()
 	{	
-		background(0);
-		drawPlayer(playerX, playerY, playerWidth);
-
-		if((frameCount % 60) == 0)
+		switch(inGame)
 		{
-			bugX = random(bugX - 20, bugX + 25);
-			bugY += 15;
+			case 0:
+				background(0);
+				splashScreen();
+				break;
+			case 1:
+				background(0);
+				drawScore();
+				drawPlayer(playerX, playerY, playerWidth);
+				moveBug();
+				drawBug(bugX, bugY , bugWidth);
+				inGameChk();
+				break;
+			case 2:
+				gameOver();
+				if (key == ' ')
+				{
+					resetGame();
+				}
+
+				break;
+
 		}
-		drawBug(bugX, bugY , bugWidth);
 	}
 
 	void drawPlayer (float x, float y, float w)
 	{
-		// create space invaders rocket
-		// body
 		fill(220);
-
 		rect(x, y, w, w * 0.5f);
 	}
 
 	void drawBug(float x, float y, float w)
 	{
-		// create space invaders bug
-		// body
 		fill(220);
 		ellipse(x, y, bugWidth, bugWidth-10);
 
@@ -78,10 +92,67 @@ public class BugZap extends PApplet
 		}
 		if (key == ' ')
 		{
-			// fire bullet
+			// draw laser
 			fill(250);
 			stroke(250);
 			line(playerX + 25, playerY - 10, playerX + 25, 0);
+
+			// if bug is hit, add to score, and reset bug
+			if (playerX + 25 > bugX - 15 && playerX + 25 < bugX + 15)
+			{
+				bugX = random(30, 470);
+				bugY = 20;
+				score ++;			
+			}
+		}
+	}
+
+	public void inGameChk()
+	{
+		if (bugY > 480)
+		{
+			inGame = 2;
+		}
+	}
+	public void moveBug()
+	{
+		if((frameCount % 60) == 0)
+		{
+			bugX = random(bugX - 20, bugX + 25);
+			bugY += 35;
+		}
+	}
+
+	public void gameOver ()
+	{
+
+		// display game over text
+		background(1);
+		fill(255);
+		textSize(20);
+		text("Game Over", 200, 250);
+		drawScore();
+	}
+	public void resetGame()
+	{
+		inGame = 1;
+		score = 0;
+		bugY = 20;
+	}
+	public void drawScore()
+	{
+		textSize(20);
+		text("Score: " + score, 410, 20);
+	}
+
+	public void splashScreen()
+	{
+		fill(255);
+		textSize(20);
+		text("Press space to start", 150, 250);
+		if (key == ' ')
+		{
+			inGame = 1;
 		}
 	}
 }
