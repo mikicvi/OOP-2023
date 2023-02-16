cd468f842e474976160f80f3cd1bf3c6c449f53f
mikicvi <vilim.mikic@gmail.com>
2023-02-16 09:32:33 +0000

    Setters and getters, encapsulation

--- java/src/ie/tudublin/Star.java
+++ java/src/ie/tudublin/Star.java
@@ -5,14 +5,24 @@ import processing.data.TableRow;
 
 public class Star {
 
-    boolean hab;
-    String displayName; 
-    float distance; 
-    float xG; 
-    float yG; 
-    float zG; 
-    float absMag;
+    private boolean hab;
+    private String displayName; 
+    private float distance; 
+    private float xG; 
+    private float yG; 
+    private float zG; 
+    private float absMag;
 
+    public Star(boolean hab, String displayName, float distance, float xG, float yG, float zG, float absMag)
+	{
+		this.hab = hab;
+		this.displayName = displayName;
+		this.distance = distance;
+		this.xG = xG;
+		this.yG = yG;
+		this.zG = zG;
+		this.absMag = absMag;
+	}
 
     public Star(TableRow tr)
     {
@@ -27,21 +37,70 @@ public class Star {
         );
     }
 
-	public Star(boolean hab, String displayName, float distance, float xG, float yG, float zG, float absMag)
-	{
-		this.hab = hab;
-		this.displayName = displayName;
-		this.distance = distance;
-		this.xG = xG;
-		this.yG = yG;
-		this.zG = zG;
-		this.absMag = absMag;
-	}
-
     @Override
-    public String toString()
-    {
-        return displayName + "\t" + distance + "\t" + xG + "\t" + yG + "\t" + zG + "\t" + absMag + "\t" + hab;
+    public String toString() {
+        return "Star [hab=" + hab + ", displayName=" + displayName + ", distance=" + distance + ", xG=" + xG + ", yG="
+                + yG + ", zG=" + zG + ", absMag=" + absMag + "]";
     }
+
+    public void setHab(boolean hab) {
+        this.hab = hab;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public void setxG(float xG) {
+        this.xG = xG;
+    }
+
+    public void setyG(float yG) {
+        this.yG = yG;
+    }
+
+    public void setzG(float zG) {
+        this.zG = zG;
+    }
+
+    public void setAbsMag(float absMag) {
+        this.absMag = absMag;
+    }
+
+    public boolean isHab() {
+        return hab;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public float getxG() {
+        return xG;
+    }
+
+    public float getyG() {
+        return yG;
+    }
+
+    public float getzG() {
+        return zG;
+    }
+
+    public float getAbsMag() {
+        return absMag;
+    }
+
+
+
+    
     
 }
