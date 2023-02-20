f1124b77b6856f3d31c4a59bc30f3a1a38f974b5
mikicvi <vilim.mikic@gmail.com>
2023-02-20 12:36:47 +0000

    Voice input

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -5,29 +5,45 @@ import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 import processing.core.PApplet;
-
+// can also do fully qyualified class name instead of import statements
+// ie. ddf.minim.AudioInput
 public class Audio1 extends PApplet
 {
 
     Minim minim; // instance of the minim library
     AudioInput ai; // instance of the AudioInput class
-    // can also do fully qyualified class name
     AudioPlayer ap;
     AudioBuffer ab;
 
 	public void settings()
 	{
-		size(500, 500);
+		size(1024, 500);
 	}
 
+    int frameSize = 1024; // number of samples in a frame
 	public void setup() 
     {
         colorMode(HSB);
         background(0);
+        minim = new Minim(this); // instanciating, creating new object, reffering to this class(current applet)(just like self in python)
+
+        ai = minim.getLineIn(Minim.MONO, frameSize, 44100, 16); // 16 bit audio
+        ab = ai.mix; // ab object encapsulates the audio buffer
+
+
+        smooth();
 	}
 
 	public void draw()
 	{	
+        background(0);
+        stroke(255);
+        
+        float half = height / 2;
+        for(int i = 0; i < ab.size(); i++)
+        {
 
+            line(i,half,i, half + ab.get(i) * half); // get(i) returns the value of the sample at index i, * half to scale it down to the height of the window
+        }
 	}
 }
