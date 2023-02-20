273403cac0916e353e3df8d21862095c37c825b4
mikicvi <vilim.mikic@gmail.com>
2023-02-20 12:08:03 +0000

    audio 1

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -26,6 +26,11 @@ public class Main
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new StarMap());
     }
+	public static void Audio1()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio1());
+    }
 	
 	public static void main(String[] args)
 	{
@@ -35,7 +40,7 @@ public class Main
 		penny.setName("Penny");
 		penny.speak();
 
-		starMap();
+		Audio1();
 	}
 	
 }
