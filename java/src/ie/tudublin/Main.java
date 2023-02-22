fccb4b8dff2784a84a542896502667e971455a68
mikicvi <vilim.mikic@gmail.com>
2023-02-22 14:54:39 +0000

    bla

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-package ie.tudublin;
-
-public class Main
-{
-
-	public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
-
-	public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
-
-	public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
-
-	public static void starMap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new StarMap());
-    }
-	
-	public static void main(String[] args)
-	{
-		System.out.println("Hello world");
-		
-		Dog penny = new Dog();
-		penny.setName("Penny");
-		penny.speak();
-
-		starMap();
-	}
-	
-}
-=======
 package ie.tudublin;
 
 public class Main
@@ -88,4 +45,3 @@ public class Main
 	}
 	
 }
->>>>>>> ea17a3ea1a4624740569d0949200140e12e2341f
