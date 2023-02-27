f5d491c5e34638c558c812f7345e2ff57d806b20
mikicvi <vilim.mikic@gmail.com>
2023-02-27 11:32:13 +0000

    init commit

---
+++ java/src/ie/tudublin/Audio2.java
@@ -0,0 +1,21 @@
+package ie.tudublin;
+
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+import processing.core.PApplet;
+
+public class Audio2 extends PApplet {
+
+    Minim m;
+    AudioInput ai;
+    AudioPlayer ap;
+    AudioBuffer ab;
+
+    public void settings()
+    {
+        size(1024, 1024);
+    }
+    
+}
