4836097ac7d4c9f9fb53812bb51144cac2011194
mikicvi <vilim.mikic@gmail.com>
2023-02-27 12:55:18 +0000

    lerped stuff and frequency

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -4,6 +4,7 @@ import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
+import ddf.minim.analysis.FFT;
 import processing.core.PApplet;
 
 public class Audio2 extends PApplet {
@@ -12,6 +13,8 @@ public class Audio2 extends PApplet {
     AudioInput ai;
     AudioPlayer ap;
     AudioBuffer ab;
+
+    FFT fft;
 
     public void settings()
     {
@@ -23,28 +26,62 @@ public class Audio2 extends PApplet {
         m = new Minim(this);
         ai = m.getLineIn(Minim.MONO, width, 44100, 16);
         ab = ai.mix;
+        lerpedBuffer = new float[width];
+
+        fft = new FFT(width, 44100);
 
     }
 
+    float[] lerpedBuffer;
+    float lerepdY = 0;
     public void draw()
     {
         background(0);
+        colorMode(HSB);
         stroke(255);
         float half = height / 2;
         for(int i = 0; i< ab.size(); i++)
         {
-            line(i, half, i, half+ab.get(i)*half);
+            stroke(map(i, 0, ab.size(), 0, 255), 255, 255);
+            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.05f);
+            float f = abs(lerpedBuffer[i] * half) * 2.0f;
+            line(i, half + f, i, half - f);
+
         }
 
-        println(map(5,0,10, 1000, 2000));
-        map1(5,0,10, 1000, 2000);
+        fft.forward(ab);
+        stroke(255);
+
+        int highestIndex = 0;
+        for(int i = 0; i < fft.specSize()/2; i++)
+        {
+            line(i*2, height, i*2, height - fft.getBand(i) * 5); 
+
+            if(fft.getBand(i) > fft.getBand(highestIndex))
+            {
+                highestIndex = i;
+            }
+        }
+        // frequency stuff
+        float freq = fft.indexToFreq(highestIndex);
+        fill(255);
+        text("Freq:" + freq, 100, 100);
+
+        float y = map(freq, 1000, 2500, height, 0);
+        lerepdY = lerp(lerepdY, y, 0.1f);
+        circle(200, y, 50);
+        circle(300, lerepdY, 50);
+        //println(map(5,0,10, 1000, 2000));
+        //println(map1(5,0,10, 1000, 2000));
     }
 
-    void map1(float a, float b, float c, float d, float e)
+    float map1(float a, float b, float c, float d, float e)
     {
-        float f = (a -b) / (c - b); // 0.5
-        float g = d + f * (e - d); // 1500
-        println(g);
+        float range1 = c - b;
+        float range2 = e - d;
+        float howFar = a - b;
+
+        return d + (howFar/range1) * range2;
     }
     
 }
