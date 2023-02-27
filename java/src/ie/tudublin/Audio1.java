f6c54be867c980028287b174ab796e9cfb79956c
mikicvi <vilim.mikic@gmail.com>
2023-02-27 11:29:52 +0000

    merged

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -53,11 +53,12 @@ public class Audio1 extends PApplet
         colorMode(HSB);
 
         y = height / 2;
-        smoothedY = y;
-
+        smoothedY = y;        
     }
 
     float off = 0;
+
+    float lerpedBuffer[] = new float[1024];
 
     public void draw()
     {
@@ -66,11 +67,14 @@ public class Audio1 extends PApplet
         float average = 0;
         float sum = 0;
         off += 1;
+
+
         // Calculate sum and average of the samples
         // Also lerp each element of buffer;
         for(int i = 0 ; i < ab.size() ; i ++)
         {
             sum += abs(ab.get(i));
+            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
         }
         average= sum / (float) ab.size();
 
@@ -88,108 +92,35 @@ public class Audio1 extends PApplet
                     //float c = map(ab.get(i), -1, 1, 0, 255);
                     float c = map(i, 0, ab.size(), 0, 255);
                     stroke(c, 255, 255);
-                    float f = ab.get(i) * halfH;
-                    line(i, halfH + f, halfH - f, i);                    
+                    float f = lerpedBuffer[i] * halfH * 4.0f;
+                    line(i, halfH + f, i, halfH - f);                    
                 }
                 break;
-            case 1:
-                // the waveform visualisation
-                background(0);
-                for(int i = 0; i < ab.size(); i++)
-                {
-                    float c = map(i, 0, ab.size(), 0, 255);
-                    stroke(c, 255, 255);
-                    float f = ab.get(i) * halfH;
-                    line(i, halfH + f, i, halfH - f);
-                }            
-                break;
-            case 2:
-            //The waveform drawn down the 4 sides of the screen
-                background(0);
-                for(int i = 0 ; i < ab.size(); i++)
-                {
-                    float c = map(i, 0, ab.size(), 0, 255);
-                    stroke(c, 255, 255);
-                    float f = ab.get(i) * halfH;
-                    
-                    //left side
-                    line(0, i, f, i);
-                    //top side
-                    line(i, 0, i, f);
-                    //right side
-                    line(width, i, width - f, i);
-                    //bottom side
-                    line(i, height, i, height - f);
-                }
-                break;
-            case 3:
-                //The circle - Use the lerped amplitude to control the size of the circle
-                background(0);
-                noFill();
-                for (int i = 0; i < ab.size(); i++)
-                {   
-                    float r = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
-                    float c = map(smoothedAmplitude, 0, 1, 0, 255);
-                    stroke(c, 255, 255);
-                    strokeWeight(3);
-                    circle(cx, cy, r);
-                }
-                break;
-            case 4:
-            // The square - Again use the lerped amplitude to control the size of the square. You can also use rectMode(CENTER) to make the x and y parameters of the rect function determine the center of the rect rather than the coordinates of the top left corner
+        case 1:
             background(0);
-            noFill();
-            for (int i = 0; i < ab.size(); i++)
-            {   
-                float r = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
-                float c = map(smoothedAmplitude, 0, 1, 0, 255);
+            for(int i = 0 ; i < ab.size() ; i ++)
+            {
+                //float c = map(ab.get(i), -1, 1, 0, 255);
+                float c = map(i, 0, ab.size(), 0, 255);
                 stroke(c, 255, 255);
-                strokeWeight(3);
-                rectMode(CENTER);
-                rect(cx, cy, r, r);
+                float f = lerpedBuffer[i] * halfH * 4.0f;
+                line(i, halfH + f, halfH - f, i);                    
             }
-            break;
-            case 5:
-            // The triangle of fixed size with an eye in middle and lines coming out of it
-            float mid = 500;
-            float height = 200;
-            float left = 150;
-            float right = 850;
-            float botheight = 800;
+            break;      
+        case 2:
             background(0);
-            noFill();
-            for (int i = 0; i < ab.size(); i++)
-            {   
-                float c = map(smoothedAmplitude, 0, 1, 0, 255);
-                float c2 = map(i, 0, ab.size(), 0, 255);
-                float f = ab.get(i) * halfH;
+            for(int i = 0 ; i < ab.size() ; i ++)
+            {
+                float c = map(i, 0, ab.size(), mouseX /2, mouseY/ 2);
                 stroke(c, 255, 255);
-                strokeWeight(3);
-                // triangle of fixed size
-                triangle(150,800, 850,800, 500,200);
-                //left side
-                stroke(c2, 255, 255);
-                strokeWeight(1);
-                line(left+350,height,left+350-abs(f),height);
-                left-=0.58;
-                if(height<800)
-                {
-                    height++;
-                }
-                //right side
-                line(right-350,height,right-350+abs(f),height);
-                right+=0.58;
-                //bottom side
-                line(mid,botheight,mid+f,botheight);
-                // line through middle (top to bottom)
-                line(mid,200,mid - f,botheight);
+                float f = lerpedBuffer[i] * halfH * 4.0f;
+                line(0, i, f, i);              
+                line(width, i, width - f, i);              
+                line(i, 0, i, f);          
+                line(i, height, i, height - f);              
             }
-            break;
-
-
-
+            break;          
         }
-        
 
 
         
