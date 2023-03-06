2b0db82128f2914ec2896ef5f3274bd9f138c6a3
mikicvi <vilim.mikic@gmail.com>
2023-03-06 11:12:50 +0000

    sd

--- java/src/ie/tudublin/Cube.java
+++ java/src/ie/tudublin/Cube.java
@@ -2,32 +2,6 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class Cube { 
-    public float x, y;
-    public float size;
-    public float c;
-    public float rot;
-
-
-    public void render(PApplet p)
-    {
-        p.stroke(c, 255,255);
-        p.strokeWeight(5);
-        p.noFill();
-        p.pushMatrix();
-        p.translate(x, y);
-
-        p.rotateX(rot);
-        p.rotateY(rot);
-
-        p.box(size);
-        p.popMatrix();
-    }
-}
-package ie.tudublin;
-
-import processing.core.PApplet;
-
 public class Cube {
     public float x, y;
     public float size;
