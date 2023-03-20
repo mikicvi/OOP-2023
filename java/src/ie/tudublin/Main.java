1fa2c82c2b51d1caa2a5d088a79fa91e3ec32db0
mikicvi <vilim.mikic@gmail.com>
2023-03-20 12:44:14 +0000

    Rotation is broken

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,70 +1,3 @@
-<<<<<<< HEAD
-package ie.tudublin;
-
-public class Main
-{
-
-	public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
-
-	public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
-
-	public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
-
-	public static void starMap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new StarMap());
-    }
-
-	public static void audio1()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Audio1());
-    }
-
-	public static void audio2()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Audio2());
-    }
-
-	public static void audio3()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Audio3());
-    }
-
-	public static void life()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Life());
-    }
-	
-	public static void main(String[] args)
-	{
-		System.out.println("Hello world");
-		
-		Dog penny = new Dog();
-		penny.setName("Penny");
-		penny.speak();
-
-		life();
-	}
-	
-}
-=======
 package ie.tudublin;
 
 public class Main
@@ -136,4 +69,3 @@ public class Main
 	}
 	
 }
->>>>>>> 192692be0846567857f1f8df49747a62619589fb
--- java/src/ie/tudublin/Ship.java
+++ java/src/ie/tudublin/Ship.java
@@ -1,18 +1,22 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import processing.core.PConstants;
 import processing.core.PVector;
 
 public class Ship {
     private PVector pos;
-    PApplet p;
+    private PVector forward;
+    private PApplet p;
     
     public Ship(float x, float y, int c, float size, PApplet p)
     {
         pos = new PVector(x, y);
+        forward = new PVector(0, -1);
         this.size = size;
         this.c = c;
         this.p = p;
+        halfSize = size / 2;
     }
 
     public PVector getPos() {
@@ -43,6 +47,55 @@ public class Ship {
     private float rot;
     private int c;
     private float size;
+    private float halfSize;
+
+    public void move()
+    {
+        forward.x = PApplet.sin(rot);
+        forward.y = PApplet.cos(-rot);
+        if(p.keyPressed)
+        {
+            if(p.keyCode == PConstants.UP)
+            {
+                pos.x -= forward.x;
+                pos.y -= forward.y;
+            }
+            if(p.keyCode == PConstants.DOWN)
+            {
+                pos.x += forward.x;
+                pos.y += forward.y;
+
+            }
+            if(p.keyCode == PConstants.LEFT)
+            {
+                rot -= 0.1f;
+            }
+            if(p.keyCode == PConstants.RIGHT)
+            {
+                rot += 0.1f;
+            }
+        }
+    }
+
+
+    public void render()
+    {
+        p.pushMatrix(); // store transfrom on top of stack
+
+        p.translate(pos.x, pos.y);
+        
+        p.rotate(rot);
+        p.stroke(c, 255, 255);
+        p.noFill();
+
+        // a ship
+        p.line(- halfSize, 0 + halfSize, 0,- halfSize);
+        p.line(0, - halfSize,  + halfSize,  + halfSize);
+        p.line(0 + halfSize, 0 + halfSize, 0, 0);
+        p.line(0, 0, 0 - halfSize, 0 + halfSize);
+        
+        p.popMatrix();
+    }
 
 
 
