cecc577e6e4bbfa97bd61c857d7e23c597376e07
mikicvi <vilim.mikic@gmail.com>
2023-03-20 11:41:24 +0000

    Setting up Ship class

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package ie.tudublin;
 
 public class Main
@@ -63,3 +64,76 @@ public class Main
 	}
 	
 }
+=======
+package ie.tudublin;
+
+public class Main
+{
+
+	public static void helloProcessing()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new HelloProcessing());
+    }
+
+	public static void loops()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Loops());
+    }
+
+	public static void bugZap()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new BugZap());
+    }
+
+	public static void starMap()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new StarMap());
+    }
+
+	public static void audio1()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio1());
+    }
+
+	public static void audio2()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio2());
+    }
+
+	public static void audio3()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio3());
+    }
+
+	public static void life()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Life());
+    }
+	
+	public static void yasc()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new YASC());
+    }
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+		
+		Dog penny = new Dog();
+		penny.setName("Penny");
+		penny.speak();
+
+		yasc();
+	}
+	
+}
+>>>>>>> 192692be0846567857f1f8df49747a62619589fb
