e2f22a9f665441c7910ae3b86f0b3e35481cebd5
mikicvi <vilim.mikic@gmail.com>
2023-03-22 16:20:32 +0000

    create AIShip subclass

---
+++ java/src/ie/tudublin/AIShip.java
@@ -0,0 +1,14 @@
+package ie.tudublin;
+
+public class AIShip extends Ship {
+
+    public AIShip(float x, float y, float size, int c, YASC p) {
+        super(x, y, size, c, p);
+    }
+
+    public void move() {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
